a54bf996f900e5f41df819bcafee5e304e4db162
Amitmishra010 <amit212.brh@gmail.com>
2025-10-01 23:50:27 +0530

    utils code discussion

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cookieParser from "cookie-parser"//(basically CRUD operation)iska kaam bas itna sa hai ki mai mere server se jo user ka browser hai uski cookie set kar pau aur access kar pau
+
+import cors from "cors"
+const app=express()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}))
+//to middleware set karna padega
+//ab kyuki data hamara kayi jagah se aa skta hai file,json,form,etc. jaise json aa raha hai to uski limit set karni hogi unlimited nahi kar sakte server crash ho jayega
+app.use(express.json({limit:"16kb"}))
+//jab url se data aata hai to thodi problem hoti hai kyuki wo encode karta hai data ko like space ko %20 kar deta hai koi koi + kar deta hai to uske liye batana padta hai express ko
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+//kayi baar ham kuch files,folder ya image ko local server per store karna chahte hai ki ye public assets hai
+app.use(express.static("public")) 
+app.use(cookieParser())
+export {app}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+ const asyncHandler=(requestHandler)=>{
+// //promises wala bhi ho skta hai aur try catch wala bhi ham promises wala use karenge
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+ }
+export {asyncHandler}
+
+
+
+
+
+
+
+
+
+//ye kuch nahi hamne ek wrapper function banaya hai jo har jagah use kar sake
+// const asyncHandler=(fn)=>async (req,res,next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code ||500).json({
+//             success:false,
+//             message: err.message
+//         })
+//     }
+// }//high order functions hote hai
